924822ddc11dfbefbe73d1fede1bceb96f1fa346
Swoffa <swoffa@gmail.com>
2013-09-15 12:38:33 +0200

    New path in KickAss.bat

--- Build Systems/KickAss.bat
+++ Build Systems/KickAss.bat
@@ -1,1 +1,3 @@
-java -jar "C:/C64/Tools/bin/KickAss.jar" %*
+java -jar "C:/C64/Tools/KickAssembler/KickAss.jar" %*
+REM Alternative usage with CLASSPATH Environment variable
+REM java "c:\banan2\" cml.kickass.KickAssembler %*
