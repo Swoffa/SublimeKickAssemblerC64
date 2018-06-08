7d962b7b8c86440c49a7328f8a59ab1ffd628ce4
Simon Oscarsson <swoffa@gmail.com>
2018-06-08 10:51:54 +0200

    Build mode as parameter instead of global

--- kickass_build.py
+++ kickass_build.py
@@ -19,14 +19,14 @@ class KickassBuildCommand(sublime_plugin.WindowCommand):
     Provide custom build variables to a build system, such as a value that needs
     to be specific to a current project.
     """
-    def createExecDict(self, sourceDict, settings):
-        global custom_var_list, buildMode
+    def createExecDict(self, sourceDict, buildMode, settings):
+        global custom_var_list
 
         # Save path variable from expansion
         tmpPath = sourceDict.pop('path', None)
 
         # Create the command
-        sourceDict['shell_cmd'] = self.createCommand(sourceDict)
+        sourceDict['shell_cmd'] = self.createCommand(sourceDict, buildMode)
 
         # Variables to expand; start with defaults, then add ours.
         useStartup = 'startup' in buildMode
@@ -51,7 +51,7 @@ class KickassBuildCommand(sublime_plugin.WindowCommand):
         else:
             return "[ -f \"bin/breakpoints.txt\" ] && cat \"bin/${build_file_base_name}.vs\" \"bin/breakpoints.txt\" > \"bin/${build_file_base_name}_MonCommands.mon\" || cat \"bin/${build_file_base_name}.vs\" > \"bin/${build_file_base_name}_MonCommands.mon\""
 
-    def createCommand(self, sourceDict):
+    def createCommand(self, sourceDict, buildMode):
         compileCommand = "java cml.kickass.KickAssembler \"${build_file_base_name}.${file_extension}\" -log \"bin/${build_file_base_name}_BuildLog.txt\" -o \"bin/${build_file_base_name}_Compiled.prg\" -vicesymbols -showmem -symbolfiledir bin"
         compileDebugCommandAdd = "-afo :afo=true :usebin=true"
         runCommand = "\"${kickass_run_path}\" -moncommands \"bin/${build_file_base_name}.vs\" \"bin/${build_file_base_name}_Compiled.prg\""
@@ -69,13 +69,12 @@ class KickassBuildCommand(sublime_plugin.WindowCommand):
         return command
 
     def run(self, **kwargs):
-        global buildMode
         buildMode = kwargs.pop('buildmode')
         settings = SublimeSettings(self)
 
         os.makedirs("bin", exist_ok=True)
 
-        self.window.run_command('exec', self.createExecDict(kwargs, settings))
+        self.window.run_command('exec', self.createExecDict(kwargs, buildMode, settings))
 
 class SublimeSettings():
     def __init__(self, parentCommand):
