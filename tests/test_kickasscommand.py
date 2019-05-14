305bfb7921f5b57f868fc83b39b946a0a2bce6e7
Swoffa <swoffa@gmail.com>
2019-05-14 09:58:17 +0200

    More KickAssCommand tests

--- tests/test_kickasscommand.py
+++ tests/test_kickasscommand.py
@@ -6,15 +6,53 @@ from module_references import kickassbuild
 
 class TestKickAssCommand(TestCase):
 
-    @patch('platform.system', return_value='Windows')
-    def test_commandText_returncommantext(self, platform_mock):
+    def test_commandText_returncommantext(self):
         settings_mock = create_autospec(kickassbuild.SublimeSettings)
         target = kickassbuild.KickAssCommand('TestCommandText', False, False, 'build')
         actual = target.CommandText
         self.assertEqual('TestCommandText', actual)
 
-    #TODO: updateEnvVars
-    #TODO: constructor
+    def test_ctor_allconditionsvalid_fieldsareset(self):
+        settings_mock = create_autospec(kickassbuild.SublimeSettings)
+        actual = kickassbuild.KickAssCommand('TestCommandText', False, True, 'build')
+        self.assertEqual('TestCommandText', actual._KickAssCommand__commandText) #Not best practice to test "private" fields...
+        self.assertEqual(False, actual._KickAssCommand__hasPreCommand)
+        self.assertEqual(True, actual._KickAssCommand__hasPostCommand)
+        self.assertEqual('build', actual._KickAssCommand__buildMode)
+
+    def test_updateEnvVars_hasprecommandisfalseandhaspostcommandisfalse_returnunchangeddict(self):
+        settings_mock = create_autospec(kickassbuild.SublimeSettings)
+        target = kickassbuild.KickAssCommand('TestCommandText', False, False, 'build')
+        sourceDict = {'env': {'a': 'b'}}
+        actual = target.updateEnvVars(sourceDict)
+        self.assertEqual({'env': {'a': 'b'}}, actual)
+
+    def test_updateEnvVars_hasprecommandistruendhaspostcommandisfalse_returdictwithenvvars(self):
+        settings_mock = create_autospec(kickassbuild.SublimeSettings)
+        target = kickassbuild.KickAssCommand('TestCommandText', True, False, 'build')
+        sourceDict = {'env': {'a': 'b'}}
+        expected = {'env': {'kickass_bin_folder': '${file_path}/${kickass_output_path}', 'kickass_prg_file': '${file_path}/${kickass_output_path}/${kickass_compiled_filename}', 'kickass_file': '${build_file_base_name}.${file_extension}', 'kickass_file_path': '${file_path}', 'a': 'b', 'kickass_buildmode': 'build'}}
+        actual = target.updateEnvVars(sourceDict)
+        print(actual)
+        self.assertEqual(expected, actual)
+
+    def test_updateEnvVars_hasprecommandisfalsendhaspostcommandistrue_returdictwithenvvars(self):
+        settings_mock = create_autospec(kickassbuild.SublimeSettings)
+        target = kickassbuild.KickAssCommand('TestCommandText', False, True, 'build')
+        sourceDict = {'env': {'a': 'b'}}
+        expected = {'env': {'kickass_bin_folder': '${file_path}/${kickass_output_path}', 'kickass_prg_file': '${file_path}/${kickass_output_path}/${kickass_compiled_filename}', 'kickass_file': '${build_file_base_name}.${file_extension}', 'kickass_file_path': '${file_path}', 'a': 'b', 'kickass_buildmode': 'build'}}
+        actual = target.updateEnvVars(sourceDict)
+        print(actual)
+        self.assertEqual(expected, actual)
+
+    def test_updateEnvVars_hasprecommandistruendhaspostcommandistrue_returdictwithenvvars(self):
+        settings_mock = create_autospec(kickassbuild.SublimeSettings)
+        target = kickassbuild.KickAssCommand('TestCommandText', True, True, 'build')
+        sourceDict = {'env': {'a': 'b'}}
+        expected = {'env': {'kickass_bin_folder': '${file_path}/${kickass_output_path}', 'kickass_prg_file': '${file_path}/${kickass_output_path}/${kickass_compiled_filename}', 'kickass_file': '${build_file_base_name}.${file_extension}', 'kickass_file_path': '${file_path}', 'a': 'b', 'kickass_buildmode': 'build'}}
+        actual = target.updateEnvVars(sourceDict)
+        print(actual)
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
