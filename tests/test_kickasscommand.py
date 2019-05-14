7271cc790d75eccfb5c9c1681f7726e664511e61
Swoffa <swoffa@gmail.com>
2019-05-14 12:21:07 +0200

    Cleanup

--- tests/test_kickasscommand.py
+++ tests/test_kickasscommand.py
@@ -33,7 +33,6 @@ class TestKickAssCommand(TestCase):
         sourceDict = {'env': {'a': 'b'}}
         expected = {'env': {'kickass_bin_folder': '${file_path}/${kickass_output_path}', 'kickass_prg_file': '${file_path}/${kickass_output_path}/${kickass_compiled_filename}', 'kickass_file': '${build_file_base_name}.${file_extension}', 'kickass_file_path': '${file_path}', 'a': 'b', 'kickass_buildmode': 'build'}}
         actual = target.updateEnvVars(sourceDict)
-        print(actual)
         self.assertEqual(expected, actual)
 
     def test_updateEnvVars_has_precommand_is_false_and_haspostcommand_is_true_returns_dict_with_envvars(self):
@@ -42,7 +41,6 @@ class TestKickAssCommand(TestCase):
         sourceDict = {'env': {'a': 'b'}}
         expected = {'env': {'kickass_bin_folder': '${file_path}/${kickass_output_path}', 'kickass_prg_file': '${file_path}/${kickass_output_path}/${kickass_compiled_filename}', 'kickass_file': '${build_file_base_name}.${file_extension}', 'kickass_file_path': '${file_path}', 'a': 'b', 'kickass_buildmode': 'build'}}
         actual = target.updateEnvVars(sourceDict)
-        print(actual)
         self.assertEqual(expected, actual)
 
     def test_updateEnvVars_has_precommand_is_true_and_haspostcommand_is_true_returns_dict_with_envvars(self):
@@ -51,7 +49,6 @@ class TestKickAssCommand(TestCase):
         sourceDict = {'env': {'a': 'b'}}
         expected = {'env': {'kickass_bin_folder': '${file_path}/${kickass_output_path}', 'kickass_prg_file': '${file_path}/${kickass_output_path}/${kickass_compiled_filename}', 'kickass_file': '${build_file_base_name}.${file_extension}', 'kickass_file_path': '${file_path}', 'a': 'b', 'kickass_buildmode': 'build'}}
         actual = target.updateEnvVars(sourceDict)
-        print(actual)
         self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
